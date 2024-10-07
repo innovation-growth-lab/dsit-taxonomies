6c412b2b6d777f2e600adb70904662b3b681de11
ampudia19 <dampudiavicente@gmail.com>
2024-10-07 16:39:50 +0200

    partitioned data for keybert

--- src/dsit_taxonomy/pipelines/data_annotation_gtr/nodes.py
+++ src/dsit_taxonomy/pipelines/data_annotation_gtr/nodes.py
@@ -3,6 +3,7 @@ This is a boilerplate pipeline 'data_annotation_gtr'
 generated using Kedro 0.19.6
 """
 
+import logging
 import pandas as pd
 from keybert import KeyBERT
 from joblib import Parallel, delayed
@@ -12,6 +13,8 @@ from .utils import (
     get_yake_keywords,
     get_keybert_keywords,
 )
+
+logger = logging.getLogger(__name__)
 
 
 def dbp_keywords(dataframe: pd.DataFrame) -> pd.DataFrame:
@@ -85,8 +88,19 @@ def keybert_keywords(dataframe: pd.DataFrame) -> pd.DataFrame:
     kw_extractor = KeyBERT("all-MiniLM-L6-v2")
     dataframe = dataframe.copy()
     dataframe["input_text"] = dataframe["title"] + " " + dataframe["abstract_text"]
-    dataframe["keybert_keywords"] = Parallel(n_jobs=8, verbose=10)(
-        delayed(get_keybert_keywords)(text, extractor=kw_extractor)
-        for text in dataframe["input_text"]
-    )
-    return dataframe[["project_id", "keybert_keywords"]]
+
+    for start in range(0, len(dataframe), 100):
+        logger.info(
+            "Processing batch %d to %d. This is number: %d / %d",
+            start,
+            start + 100,
+            start // 100,
+            len(dataframe) // 100,
+        )
+        end = start + 100
+        batch_df = dataframe.iloc[start:end]
+        batch_df.loc[:, "keybert_keywords"] = Parallel(n_jobs=8, verbose=10)(
+            delayed(get_keybert_keywords)(text, extractor=kw_extractor)
+            for text in batch_df["input_text"]
+        )
+        yield {f"s{int(start/100)}": batch_df[["project_id", "keybert_keywords"]]}
