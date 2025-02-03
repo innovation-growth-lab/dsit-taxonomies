79c6041748bdf917e8e3b01780aea701fc6d46b7
ampudia19 <dampudiavicente@gmail.com>
2025-02-03 10:31:57 +0100

    linting and style fix

--- src/dsit_taxonomy/pipelines/keyword_similarity_validation/pipeline.py
+++ src/dsit_taxonomy/pipelines/keyword_similarity_validation/pipeline.py
@@ -4,11 +4,10 @@ generated using Kedro 0.19.10
 """
 
 from kedro.pipeline import Pipeline, pipeline, node
-from .nodes import (
-    select_sample_projects, get_expert_labels
-)
+from .nodes import select_sample_projects, get_expert_labels
 
-def create_pipeline(**kwargs) -> Pipeline:
+
+def create_pipeline(**kwargs) -> Pipeline:  # pylint: disable=C0116,W0613
     sample_selection_pipeline = pipeline(
         [
             node(
@@ -19,7 +18,10 @@ def create_pipeline(**kwargs) -> Pipeline:
             ),
             node(
                 func=get_expert_labels,
-                inputs={"taxonomy": "taxonomy.goscience.bottom.db", "data": "gtr.projects.sample"},
+                inputs={
+                    "taxonomy": "taxonomy.goscience.bottom.db",
+                    "data": "gtr.projects.sample",
+                },
                 outputs="gtr.projects.sample.expert_labels",
                 name="get_expert_labels",
             ),
